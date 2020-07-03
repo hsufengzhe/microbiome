478ca31f7b0cfae0a4ea9888407b034265314567
hsufengzhe <61205179+hsufengzhe@users.noreply.github.com>
2020-07-03 15:00:32 +0800

    Update statistical analyses.r

--- statistical analyses.r
+++ statistical analyses.r
@@ -38,10 +38,84 @@ df=t(data2)
 t<-pamk(df,krange=2:20,criterion="asw",usepam=T,ns=20,critout=TRUE,diss=F)
 pam.res<-pam(df,the_optimum_number)
 
- #scripts for figures
+#scripts for figures
 fviz_cluster(pam.res,df,repel =T, pointsize =4,labelsize = 20, main = " ",show.clust.cent = F)+labs(title = "Guangzhou Nutrition and Health Study Cohort")+theme(axis.title.x=element_text(size=24),axis.title.y=element_text(size=24),plot.title=element_text(size=24))+theme(legend.text = element_text(size = 18))+theme(legend.title = element_text(size = 18))+ theme(axis.text.x = element_text(size = 18))+ theme(axis.text.y = element_text(size = 18))
 fviz_cluster(pam.res,df,repel =T, pointsize =4,labelsize = 20, main = " ",show.clust.cent = F)+labs(title = "Replication Cohort")+theme(axis.title.x=element_text(size=24),axis.title.y=element_text(size=24),plot.title=element_text(size=24))+theme(legend.text = element_text(size = 18))+theme(legend.title = element_text(size = 18))+ theme(axis.text.x = element_text(size = 18))+ theme(axis.text.y = element_text(size = 18))
 fviz_cluster(pam.res,df,repel =T, pointsize =4,labelsize = 20, main = " ",show.clust.cent = F)+labs(title = "Antibiotic-taking Group")+theme(axis.title.x=element_text(size=24),axis.title.y=element_text(size=24),plot.title=element_text(size=24))+theme(legend.text = element_text(size = 18))+theme(legend.title = element_text(size = 18))+ theme(axis.text.x = element_text(size = 18))+ theme(axis.text.y = element_text(size = 18))
+
+#scripts for Jaccard similarity coefficient
+data=read.csv("discovery_genus_pathway.csv", header=T, row.names=1)
+df=t(data)
+pam.res<-pam(df,5)
+c1<-pam.res$clustering
+
+data=read.csv("replication_genus_pathway.csv", header=T, row.names=1)
+df=t(data)
+pam.res<-pam(df,5)
+c2<-pam.res$clustering
+
+data=read.csv("antibiotic.csv", header=T, row.names=1)
+df=t(data)
+pam.res<-pam(df,5)
+c3<-pam.res$clustering
+
+c11<-as.numeric(c1)
+c22<-as.numeric(c2)
+c33<-as.numeric(c3)
+
+m<-1
+result1<-data.frame()
+for (i in 1:21) {
+  for (j in (i+1):22) {
+    result1[m,1]<-i
+    result1[m,2]<-j
+    if(c11[i]==c11[j])
+      result1[m,3]<-1
+    else result1[m,3]<-0
+    m<-m+1
+  }
+  
+}
+
+m<-1
+result2<-data.frame()
+for (i in 1:21) {
+  for (j in (i+1):22) {
+    result2[m,1]<-i
+    result2[m,2]<-j
+    if(c22[i]==c22[j])
+      result2[m,3]<-1
+    else result2[m,3]<-0
+    m<-m+1
+  }
+  
+}
+
+m<-1
+result3<-data.frame()
+for (i in 1:21) {
+  for (j in (i+1):22) {
+    result3[m,1]<-i
+    result3[m,2]<-j
+    if(c33[i]==c33[j])
+      result3[m,3]<-1
+    else result3[m,3]<-0
+    m<-m+1
+  }
+  
+}
+ss<-0
+sd<-0
+ds<-0
+dd<-0
+for (i in 1:231) {
+ if(result1[i,3]==1 && result2[i,3]==1) ss<-ss+1
+ if(result1[i,3]==1 && result2[i,3]==0) sd<-sd+1
+ if(result1[i,3]==0 && result2[i,3]==1) ds<-ds+1
+ if(result1[i,3]==0 && result2[i,3]==0) dd<-dd+1
+}
+ss+sd+ds+dd
+jaccard_fh=ss/(ss+sd+ds)
 
 #scripts for spieceasi
 metabolites<-data.matrix(round(data[,c(3022,3026,3032,3033,3061,3079,3084,3087,3135,3138,3139,3141,3188,3189,3216,3219,3230,3261,3276,3278,3279,3287)]*100000))
